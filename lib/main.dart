d88de0ff080ca013aa7c9c8ded95558ab87c0a0d
iseruuuuu <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>
2023-03-25 13:59:58 +0900

    add: 初期設定を作成

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:daily_house_work_app/screen/tab_screen.dart';
+import 'package:daily_house_work_app/service/storage_service.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageService.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TabScreen(),
+    );
+  }
+}
